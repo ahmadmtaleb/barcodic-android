65cd634f7ada6f5ea6235c387bcb96877c1c5f9b
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2020-07-21 13:09:11 +0300

    working scan

--- app/src/main/java/com/example/android/barcodic/MainActivity.java
+++ app/src/main/java/com/example/android/barcodic/MainActivity.java
@@ -9,6 +9,10 @@ import android.view.KeyboardShortcutGroup;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,14 +36,26 @@ public class MainActivity extends AppCompatActivity {
         // Find the View that redirect to camera to scan barcode
         ImageView single = (ImageView) findViewById(R.id.single_scan);
 
-        // Set a click listener on that View
         single.setOnClickListener(new View.OnClickListener() {
             // The code in this method will be executed when the numbers View is clicked on.
             @Override
             public void onClick(View view) {
-                Intent singleIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivity(singleIntent);
+                IntentIntegrator scanner = new IntentIntegrator(MainActivity.this);
+                scanner.initiateScan();
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }

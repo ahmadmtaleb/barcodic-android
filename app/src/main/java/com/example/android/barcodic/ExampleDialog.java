583193e3c7c18cc748190e38ca17c34f02803446
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2020-07-23 23:37:34 +0300

    not working fetch

---
+++ app/src/main/java/com/example/android/barcodic/ExampleDialog.java
@@ -0,0 +1,49 @@
+package com.example.android.barcodic;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.app.AlertDialog;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class ExampleDialog extends AppCompatDialogFragment {
+//    MainActivity MainActivity = new MainActivity();
+//    String var = MainActivity.getVariable();
+    public static ExampleDialog newInstance(String msg) {
+                ExampleDialog fragment = new ExampleDialog();
+
+                Bundle bundle = new Bundle();
+                bundle.putString("msg", msg);
+                fragment.setArguments(bundle);
+
+                return fragment;
+            }
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        String price = "7,500";
+        String currency = " L.L";
+        String english_name = "blah1";
+        String arabic_name = "blah2";
+
+        final MainActivity main = (MainActivity) getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(price + currency)
+                .setMessage(getArguments().getString("msg"))
+//                .setMessage("Name: " +english_name+"\n"+"Name: "+arabic_name+"\n")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                    }
+                });
+        return builder.create();
+
+    }
+
+}
--- app/src/main/java/com/example/android/barcodic/MainActivity.java
+++ app/src/main/java/com/example/android/barcodic/MainActivity.java
@@ -5,16 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.KeyboardShortcutGroup;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.Response.Listener;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
+    public static String myVariable;
+    private RequestQueue mQueue;
+    public static String english_name;
+    public static String arabic_name;
+    public static String price;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
         });
         // Find the View that redirect to camera to scan barcode
         ImageView single = (ImageView) findViewById(R.id.single_scan);
+        //added for the volley library
 
         single.setOnClickListener(new View.OnClickListener() {
             // The code in this method will be executed when the numbers View is clicked on.
@@ -42,14 +60,53 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 IntentIntegrator scanner = new IntentIntegrator(MainActivity.this);
                 scanner.initiateScan();
+                jsonParse();
+                openDialog();
+
             }
         });
     }
+    private void jsonParse() {
+        mQueue = Volley.newRequestQueue(this);
+        String url = "http://192.168.1.10:8000/api/items-barcode/"+myVariable;
+//        String url = "http://192.168.1.255:8000/api/items-barcode/"+myVariable;
+//        String url = "http://172.17.0.1:8000/api/items-barcode/"+myVariable;
+//        String url = "http://10.0.2.2:8000/api/items-barcode/"+myVariable;
+//        String url = "http://127.0.0.1:8000/api/items-barcode/"+myVariable;
+
+        Log.e("fetchedUrl", url);
+        Log.e("jsonParseFetched1",english_name+arabic_name+price);
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                response -> {
+                    try {
+                        JSONArray jsonArray = response.getJSONArray("data");
+                        JSONObject item = jsonArray.getJSONObject(0);
+                            english_name = item.getString("english_name");
+                            arabic_name = item.getString("arabic_name");
+                            price = item.getString("price");
+                            Log.e("jsonParseFetched2",english_name+arabic_name+price);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Log.e("catchErrorException", "catched Error what the fuck");
+                    }
+                }, error -> error.printStackTrace());
+        mQueue.add(request);
+}
+
+    public void openDialog() {
+//        ExampleDialog exampleDialog = new ExampleDialog();
+        ExampleDialog exampleDialog = new ExampleDialog().newInstance(myVariable);
+        exampleDialog.show(getSupportFragmentManager(), "example dialog");
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if(result != null) {
-            if(result.getContents() == null) {
+        myVariable = result.getContents();
+
+        if (result != null) {
+            if (result.getContents() == null) {
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
             } else {
                 Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
